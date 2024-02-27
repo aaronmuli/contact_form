61470a8e9881e9d46b2e00f0bcceee239fa03bf0
AaronMuli <zionmurlley@gmail.com>
2024-02-27 12:24:05 +0200

    added form

--- includes/contact-form.php
+++ includes/contact-form.php
@@ -1,7 +1,39 @@
 <?php
 
 add_shortcode("contact","show_contact_form");
+add_action('rest_api_init', 'create_rest_endpoint');
+add_action('init', 'contact_custom_post');
 
 function show_contact_form() {
-    return "this is where the form will show!";
+    include 'templates/form.php';
+}
+
+function create_rest_endpoint() {
+    register_rest_route('/contact_form/v1', '/submit', array(
+        'methods' => 'POST',
+        'callback' => 'handle_enq'
+    ));
+
+    function handle_enq($data) {
+
+        $params = $data -> get_params();
+
+        foreach ($params as $key => $value) {
+            echo $key . " : " . $value . "\n";
+        }
+
+    }
+}
+
+function contact_custom_post() {
+    register_post_type('wporg_submissions',
+		array(
+			'labels'      => array(
+				'name'          => __('Submissions', 'textdomain'),
+				'singular_name' => __('Submission', 'textdomain'),
+			),
+				'public'      => true,
+				'has_archive' => true,
+		)
+	);
 }
---
+++ includes/templates/form.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+</head>
+<body>
+    <form method="post">
+    <label for="name">Name</label><br/>
+    <input type="text" name="name" id="name" placeholder="name"><br/>
+    
+    <label for="email">Email</label><br/>
+    <input type="email" name="email" id="email" placeholder="email"><br/>
+    
+    <label for="phone">Phone</label><br/>
+    <input type="tel" name="phone" id="phone" placeholder="phone"><br/>
+    
+    <label for="message">Message</label><br/>
+    <textarea name="message" id="message" cols="30" rows="10" placeholder="your message"></textarea><br/>
+
+    <button id="submit" type="submit">send message</button>
+</form>
+<script>
+    
+    document.getElementById("submit").addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const form = document.querySelector('form');
+
+        let formData = new FormData(form);
+        
+        fetch("<?php echo get_rest_url() . 'contact_form/v1/submit'?>", {
+            method: "POST",
+            body: formData
+        });
+    });
+
+</script>
+</body>
+</html>
